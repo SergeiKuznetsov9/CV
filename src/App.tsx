db73382645bb42b03ba17dd0ad25661895fcdbb5
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-06-21 17:09:52 +0300

    задана начальная структура

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { MainPage } from "./pages/main/MainPage";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
 }
 
 export default App;

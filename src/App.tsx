e548308867369232827d8f5fe8c4db883717ac62
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-06-25 17:47:41 +0300

    корректировка стилей

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,8 @@ import { Header } from "./widgets/header/header";
 export const App = () => {
   return (
     <div className={cls.app}>
-      <div className={cls.blur}>
-        <Header className={cls.header} />
-        <MainPage />
-      </div>
+      <Header className={cls.header} />
+      <MainPage />
     </div>
   );
 };

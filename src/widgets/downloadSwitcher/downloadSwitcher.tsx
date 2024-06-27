ae6a1693e692d5aef80ac2a81d6d037a2a1c3a3d
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-06-27 16:50:10 +0300

    скачивание cv

--- src/widgets/downloadSwitcher/downloadSwitcher.tsx
+++ src/widgets/downloadSwitcher/downloadSwitcher.tsx
@@ -1,13 +1,17 @@
 import { useTranslation } from "react-i18next";
 import cls from "./downloadSwitcher.module.scss";
+import { useContext } from "react";
+import { ThemeContext, ThemeContextProps } from "../../providers/themeProvider";
 
 export const DownloadSwitcher = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const { theme } = useContext(ThemeContext) as ThemeContextProps;
 
   const downloadFile = () => {
+    const fileName = `cv-${theme}-${i18n.language}.pdf`;
     const link = document.createElement("a");
-    link.href = "/cv.pdf";
-    link.download = "cv.pdf";
+    link.href = `/${fileName}`;
+    link.download = fileName;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);

376639b3e44c5cd0496fba691bdf20d917ca59c5
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-07-03 18:29:31 +0300

    добавить переводы

--- src/widgets/softSkills/softSkills.tsx
+++ src/widgets/softSkills/softSkills.tsx
@@ -5,17 +5,19 @@ import cls from "./softSkills.module.scss";
 export const SoftSkills = () => {
   const { t } = useTranslation();
   return (
-    <section className={cls.Education}>
+    <section className={cls.SoftSkills}>
       <StickerLine text={t("мягкие навыки")} className={cls.sticker} />
       <div className={cls.state}>
-        <ul>
-          <li>Внимательность</li>
-          <li>Ответственность</li>
-          <li>Пунктуалность</li>
-          <li>Хороший уровень коммуникации</li>
-          <li>Умение работать в команде</li>
-          <li>Нацеленность на саморазвитие в выбранной сфере, в т.ч. постоянное обучение</li>
-        </ul>
+        <p className={cls.item}>{t("Внимательность")}</p>
+        <p className={cls.item}>{t("Ответственность")}</p>
+        <p className={cls.item}>{t("Пунктуалность")}</p>
+        <p className={cls.item}>{t("Хороший уровень коммуникации")}</p>
+        <p className={cls.item}>{t("Умение работать в команде")}</p>
+        <p className={cls.item}>
+          {t(
+            "Нацеленность на саморазвитие в выбранной сфере, в т.ч. постоянное обучение"
+          )}
+        </p>
       </div>
     </section>
   );

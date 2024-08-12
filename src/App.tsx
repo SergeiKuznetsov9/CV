3aec2f5b4428197802df898f4802ae0b588d8ed2
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2024-08-12 17:31:59 +0300

    добавлен список пет проектов

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { MainPage } from "./pages/main/MainPage";
 import cls from "./app.module.scss";
 import { Header } from "./widgets/header/header";
+import { PetProjectPage } from "./pages/petProjects/PetProjectsPage";
 
 export const App = () => {
   return (
     <div className={cls.app}>
       <Header className={cls.header} />
       <MainPage />
+      <PetProjectPage />
     </div>
   );
 };
